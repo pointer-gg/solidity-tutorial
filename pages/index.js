61b9272973bf72b5f654537c79664a00065032bf
Kenneth Cassel <kennethcassel@gmail.com>
2021-12-14 10:52:22 -0600

    add keyboard images

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,28 @@
-import Head from 'next/head';
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className="">
-      <main className="max-w-3xl">
-        <h1 className="text-4xl mt-48 text-center">
+    <div className=''>
+      <main className='max-w-3xl mx-auto'>
+        <h1 className='mt-48 text-4xl text-center'>
           Solidity Keyboard Generator
         </h1>
-        <img className="filter saturate-150" src="/keeb.png" />
+        <img
+          className='h-64 mx-auto mt-8'
+          src='keyboards/eighty-percent/ABS.png'
+        />
       </main>
 
-      <footer className="mx-auto text-center mt-48">
+      <footer className='mx-auto mt-48 text-center'>
         <a
-          href="https://www.pointer.gg?utm_source=stackblitz-solidity"
-          target="_blank"
-          rel="noopener noreferrer"
+          href='https://www.pointer.gg?utm_source=stackblitz-solidity'
+          target='_blank'
+          rel='noopener noreferrer'
         >
-          Learn web3 dev and earn crypto rewards at{' '}
-          <span className="">Pointer</span>
+          Learn web3 dev and earn crypto rewards at{" "}
+          <span className=''>Pointer</span>
         </a>
+        <p>Art from Joanne Li @joanne on Figma keeybs.com</p>
       </footer>
     </div>
   );
