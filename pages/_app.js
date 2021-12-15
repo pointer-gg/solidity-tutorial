b250c0aa1d1e1d690ab065564cc31933757200dd
Callum McIntyre <callum@pointer.gg>
2021-12-15 11:43:02 +0000

    Add initial wallet connect flow

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,7 @@
-import 'tailwindcss/tailwind.css';
-import '../styles/globals.css';
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return <Component {...pageProps} />
 }
 
-export default MyApp;
+export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,68 @@
-import Head from "next/head";
+import { useState, useEffect } from "react";
+import PrimaryButton from "../components/primary-button";
 
 export default function Home() {
+
+  const [ethereum, setEthereum] = useState(undefined);
+  const [connectedAccount, setConnectedAccount] = useState(undefined);
+
+  const handleAccounts = (accounts) => {
+    if (accounts.length > 0) {
+      const account = accounts[0];
+      console.log('We have an authorized account: ', account);
+      setConnectedAccount(account);
+    } else {
+      console.log("No authorized accounts yet")
+    }
+  };
+
+  const getConnectedAccount = async () => {
+    if (window.ethereum) {
+      setEthereum(window.ethereum);
+    }
+
+    if (ethereum) {
+      const accounts = await ethereum.request({ method: 'eth_accounts' });
+      handleAccounts(accounts);
+    }
+  };
+  useEffect(() => getConnectedAccount(), []);
+
+  const connectAccount = async () => {
+    if (!ethereum) {
+      alert('MetaMask is required to connect an account');
+      return;
+    }
+
+    const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+    handleAccounts(accounts);
+  };
+
+  const renderKeyboards = () => {
+    if(!ethereum) {
+      return <p>Please install MetaMask to connect to this site</p>
+    }
+
+    if(!connectedAccount) {
+      return <PrimaryButton onClick={connectAccount}>Connect MetaMask Wallet</PrimaryButton>
+    }
+
+    return <p>Connected Account: {connectedAccount}</p>
+  }
+
   return (
     <div className=''>
       <main className='max-w-3xl mx-auto'>
-        <h1 className='mt-48 text-4xl text-center'>
+        <h1 className='mt-48 text-4xl'>
           Solidity Keyboard Generator
         </h1>
-        <img
+        {/* <img
           className='h-64 mx-auto mt-8'
           src='keyboards/eighty-percent/ABS.png'
-        />
+        /> */}
+
+        {renderKeyboards()}
+
       </main>
 
       <footer className='mx-auto mt-48 text-center'>
@@ -22,7 +74,7 @@ export default function Home() {
           Learn web3 dev and earn crypto rewards at{" "}
           <span className=''>Pointer</span>
         </a>
-        <p>Art from Joanne Li @joanne on Figma keeybs.com</p>
+        <p>Art from Joanne Li @joanne on Figma <a href='keeybs.com' className='underline'>keeybs.com</a> <a href='https://creativecommons.org/licenses/by/4.0/' className="underline">CC 4.0</a></p>
       </footer>
     </div>
   );
