c7cd966f8afb9454067d872fccfa56238f1355f6
Callum McIntyre <callum@pointer.gg>
2021-12-17 14:36:56 +0000

    Check connected chain is rinkeby, reload if chain changes

--- components/meta-mask-account-provider.js
+++ components/meta-mask-account-provider.js
@@ -6,9 +6,17 @@ export default function MetaMaskAccountProvider({children}) {
   const [ethereum, setEthereum] = useState(undefined);
   const [connectedAccount, setConnectedAccount] = useState(undefined);
   
-  const setEthereumFromWindow = () => {
+  const setEthereumFromWindow = async () => {
     if(window.ethereum) {
-      setEthereum(window.ethereum);
+      // Reload if chain changes, see https://docs.metamask.io/guide/ethereum-provider.html#chainchanged
+      window.ethereum.on('chainChanged', (_chainId) => window.location.reload());
+      const chainId = await window.ethereum.request({ method: 'eth_chainId' });
+      const rinkebyId = '0x4'; // See https://docs.metamask.io/guide/ethereum-provider.html#chain-ids
+      if(chainId === rinkebyId) {
+        setEthereum(window.ethereum);
+      } else {
+        alert('Please use Rinkeby network');
+      }
     }
   }
   useEffect(setEthereumFromWindow, [])
