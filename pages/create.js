770aa5db6467c85255f0d1cf7bf1a6db0d9435bd
Callum McIntyre <callum@pointer.gg>
2021-12-17 11:36:52 +0000

    Extract Footer component

--- pages/create.js
+++ pages/create.js
@@ -4,6 +4,7 @@ import PrimaryButton from "../components/primary-button";
 import Router from 'next/router'
 import getKeyboardsContract from "../utils/getKeyboardsContract";
 import { useMetaMaskAccount } from "../components/meta-mask-account-provider";
+import Footer from "../components/footer";
 
 export default function Create() {
   const { ethereum, connectedAccount, connectAccount } = useMetaMaskAccount();
@@ -122,18 +123,7 @@ export default function Create() {
         {renderCreateForm()}
 
       </main>
-
-      <footer className='mx-auto mt-48 text-center'>
-        <a
-          href='https://www.pointer.gg?utm_source=stackblitz-solidity'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn web3 dev and earn crypto rewards at{" "}
-          <span className=''>Pointer</span>
-        </a>
-        <p>Art from Joanne Li @joanne on Figma <a href='keeybs.com' className='underline'>keeybs.com</a> <a href='https://creativecommons.org/licenses/by/4.0/' className="underline">CC 4.0</a></p>
-      </footer>
+      <Footer />
     </div>
   );
 }
