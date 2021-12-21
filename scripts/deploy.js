e07de4d3edaa17a578d1a5d94bc747d1290e66dc
Callum McIntyre <callum@pointer.gg>
2021-12-21 14:19:57 +0000

    First version of the contract, using plain strings

---
+++ scripts/deploy.js
@@ -0,0 +1,19 @@
+async function main() {
+  const keyboardsContractFactory = await hre.ethers.getContractFactory("Keyboards");
+  const keyboardsContract = await keyboardsContractFactory.deploy();
+  await keyboardsContract.deployed();
+
+  console.log("The keyboards contract is deployed!", keyboardsContract.address)
+
+  const keyboards = await keyboardsContract.getKeyboards();
+  console.log("We got the keyboards!", keyboards);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
