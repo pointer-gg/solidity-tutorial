71b7dcca025296497e414f20d2f42f7b4d355f7e
Callum McIntyre <callum@pointer.gg>
2021-12-22 16:54:43 +0000

    Update the contract to use a Keyboard struct

--- contracts/Keyboards.sol
+++ contracts/Keyboards.sol
@@ -2,13 +2,38 @@
 pragma solidity ^0.8.4;
 
 contract Keyboards {
-  string[] public createdKeyboards;
+  enum KeyboardKind {
+    SixtyPercent,
+    SeventyFivePercent,
+    EightyPercent,
+    Iso105
+  }
 
-  function getKeyboards() view public returns(string[] memory) {
+  struct Keyboard {
+    KeyboardKind kind;
+    // ABS = false, PBT = true
+    bool isPBT;
+    // tailwind filters to layer over
+    string filter;
+  }
+
+  Keyboard[] public createdKeyboards;
+
+  function getKeyboards() view public returns(Keyboard[] memory) {
     return createdKeyboards;
   }
 
-  function create(string calldata _description) external {
-    createdKeyboards.push(_description);
+  function create(
+    KeyboardKind _kind,
+    bool _isPBT,
+    string calldata _filter
+  ) external {
+    Keyboard memory newKeyboard = Keyboard({
+      kind: _kind,
+      isPBT: _isPBT,
+      filter: _filter
+    });
+
+    createdKeyboards.push(newKeyboard);
   }
 }
