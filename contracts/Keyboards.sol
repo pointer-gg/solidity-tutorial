f49e4c57130118be2340054bcc4e4d04d74ae8f1
Callum McIntyre <callum@pointer.gg>
2021-12-23 12:31:08 +0000

    Add tip function to the contract

--- contracts/Keyboards.sol
+++ contracts/Keyboards.sol
@@ -15,6 +15,8 @@ contract Keyboards {
     bool isPBT;
     // tailwind filters to layer over
     string filter;
+    // user who created it!
+    address owner;
   }
 
   Keyboard[] public createdKeyboards;
@@ -31,9 +33,15 @@ contract Keyboards {
     Keyboard memory newKeyboard = Keyboard({
       kind: _kind,
       isPBT: _isPBT,
-      filter: _filter
+      filter: _filter,
+      owner: msg.sender
     });
 
     createdKeyboards.push(newKeyboard);
+  }
+
+  function tip(uint256 _index) external payable {
+    address payable owner = payable(createdKeyboards[_index].owner);
+    owner.transfer(msg.value);
   }
 }
