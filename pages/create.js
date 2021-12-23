8d61852afe097ae48f497e695a3b505e224dc2a0
Callum McIntyre <callum@pointer.gg>
2021-12-23 14:50:52 +0000

    Update the contract address + abi

--- pages/create.js
+++ pages/create.js
@@ -16,7 +16,7 @@ export default function Create() {
 
   const [mining, setMining] = useState(false);
 
-  const contractAddress = '0x297D3fBfF8D755cd12B322028498AF36Bf3A7490';
+  const contractAddress = '0xe3a6ECEA5217D4213964eB734eae0A49BCea6De9';
   const contractABI = abi.abi;
 
   const handleAccounts = (accounts) => {
