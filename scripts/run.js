413574badf93b59242466d6ba835ec7fe83964ba
Callum McIntyre <callum@pointer.gg>
2021-12-23 14:46:19 +0000

    Update the contract to emit keyboard created + tip sent events

--- scripts/run.js
+++ scripts/run.js
@@ -5,20 +5,12 @@ async function main() {
   const keyboardsContract = await keyboardsContractFactory.deploy();
   await keyboardsContract.deployed();
 
-  const keyboardTxn1 = await keyboardsContract.create(0, true, "sepia");
-  await keyboardTxn1.wait();
+  const keyboardTxn = await keyboardsContract.create(0, true, "sepia");
+  await keyboardTxn.wait();
 
-  const keyboardTxn2 = await keyboardsContract.connect(somebodyElse).create(1, false, "grayscale");
-  await keyboardTxn2.wait();
-
-  const balanceBefore = await hre.ethers.provider.getBalance(somebodyElse.address);
-  console.log("somebodyElse balance before!", hre.ethers.utils.formatEther(balanceBefore));
-
-  const tipTxn = await keyboardsContract.tip(1, {value: hre.ethers.utils.parseEther("1000")}); // tip the 2nd keyboard as owner!
-  await tipTxn.wait();
-
-  const balanceAfter = await hre.ethers.provider.getBalance(somebodyElse.address)
-  console.log("somebodyElse balance after!", hre.ethers.utils.formatEther(balanceAfter));
+  const tipTxn = await keyboardsContract.connect(somebodyElse).tip(0, {value: hre.ethers.utils.parseEther("1")})
+  const tipTxnReceipt = await tipTxn.wait();
+  console.log(tipTxnReceipt.events);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
