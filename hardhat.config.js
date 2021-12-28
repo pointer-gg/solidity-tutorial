aca06b2e9724e30f2b00f01c991c2fdf7e160718
Kenneth Cassel <kennethcassel@gmail.com>
2021-12-28 11:26:11 -0600

    fix: remove network from exports to match tutorial

--- hardhat.config.js
+++ hardhat.config.js
@@ -18,10 +18,4 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
-  networks: {
-    rinkeby: {
-      url: process.env.ALCHEMY_API_URL,
-      accounts: [process.env.RINKEBY_PRIVATE_KEY],
-    },
-  },
 };
