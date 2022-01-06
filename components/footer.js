22e8fbc5aad46d5393a434fe8bb19dbd1b7ae7c7
Callum McIntyre <callum@pointer.gg>
2022-01-06 15:57:22 +0000

    Fixup: use absolute link for keeybs in footer

--- components/footer.js
+++ components/footer.js
@@ -9,7 +9,7 @@ export default function Footer() {
         Learn web3 dev and earn crypto rewards at{" "}
         <span className=''>Pointer</span>
       </a>
-      <p>Art from Joanne Li @joanne on Figma <a href='keeybs.com' className='underline'>keeybs.com</a> <a href='https://creativecommons.org/licenses/by/4.0/' className="underline">CC 4.0</a></p>
+      <p>Art from Joanne Li @joanne on Figma <a href='https://keeybs.com' className='underline'>keeybs.com</a> <a href='https://creativecommons.org/licenses/by/4.0/' className="underline">CC 4.0</a></p>
     </footer>
   )
 }
